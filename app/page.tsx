6160f76381603f99aa4b02c5abb51b4ef39f6ec8
MANZI RURANGIRWA Elvis <elvismanzi16@gmail.com>
2025-12-24 13:18:34 +0200

    ...

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,4 @@
 import { redirect } from "next/navigation"
-const res = await fetch("http://localhost:9090/login", {
-  method: "POST",
-  headers: { "Content-Type": "application/json" },
-  body: JSON.stringify({ email, password }),
-});
-
-const data = await res.json();
-localStorage.setItem("token", data.token);
-router.push("/dashboard");
-
 export default function Home() {
   redirect("/auth/login")
 }
