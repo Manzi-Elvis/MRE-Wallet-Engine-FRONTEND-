605067e995e8902c20d1137aad9b909994a4d80d
MANZI RURANGIRWA Elvis <elvismanzi16@gmail.com>
2025-12-24 13:16:12 +0200

    ...

--- middleware.ts
+++ middleware.ts
@@ -1,9 +1,11 @@
-import { NextResponse } from 'next/server';
+import { NextRequest, NextResponse } from "next/server";
 
-export function middleware(req) {
-  const token = req.cookies.get('token');
+export function middleware(req: NextRequest) {
+  const token = req.cookies.get("token");
 
-  if (!token && req.nextUrl.pathname.startsWith('/dashboard')) {
-    return NextResponse.redirect(new URL('/login', req.url));
+  if (!token && req.nextUrl.pathname.startsWith("/dashboard")) {
+    return NextResponse.redirect(new URL("/login", req.url));
   }
+
+  return NextResponse.next();
 }
